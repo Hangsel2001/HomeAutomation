718bcffdb1e9b0be82e418c46c9873cfbe2aee6b
hangsel2001 <jag@markushangsel.com>
2015-07-30 22:44:59 +0200

    Lagt till Arduinokoden för verkstadslösningen.

---
+++ XbeeTempAlerter/XbeeTempAlerter.ino
@@ -0,0 +1,138 @@
+//    FILE: dht_test.ino
+//  AUTHOR: Rob Tillaart
+// VERSION: 0.1.07
+// PURPOSE: DHT Temperature & Humidity Sensor library for Arduino
+//     URL: http://arduino.cc/playground/Main/DHTLib
+//
+// Released to the public domain
+//
+
+#include "dht.h"
+#include <LiquidCrystal.h>
+#include "time.h"
+#include <string.h>
+#include <EEPROM.h>
+
+dht DHT;
+LiquidCrystal lcd(3, 4, 5, 6, 7, 8);
+#define DHT22_PIN 2
+#define LOCATION "Verkstad"
+#define BUTTON_PIN 9
+#define HOUR_ADJUST 2
+
+volatile double temperature;
+volatile double humidity;
+volatile int status = 0;
+unsigned long lastRead = 0;
+unsigned long lastChange = 0;
+volatile unsigned int lastSecond = 0;
+
+void setup()
+{
+	lcd.begin(16, 2);
+	Serial.begin(9600);
+	Serial.println("DHT TEST PROGRAM ");
+	Serial.print("LIBRARY VERSION: ");
+	Serial.println(DHT_LIB_VERSION);
+	Serial.println();
+	Serial.println("Type,\tstatus,\tHumidity (%),\tTemperature (C)");
+	Serial1.begin(9600);
+	Serial1.write("U");
+}
+
+void printDigits(int digits) {
+	// utility function for digital clock display: prints preceding colon and leading 0
+	if (digits < 10)
+		lcd.print('0');
+	lcd.print(digits);
+}
+
+void updateLCD() {
+	if (second() != lastSecond) {
+		lcd.setCursor(0, 0);
+		printDigits(hour());
+		lcd.print(":");
+		printDigits(minute());
+		lcd.print(":");
+		printDigits(second());
+
+		char temp[6];
+		dtostrf(temperature, 6, 1, temp);
+
+
+		lcd.print(temp);
+
+		lcd.print((char)223);
+		lcd.print("C");
+
+		lcd.setCursor(0, 1);
+		lcd.print(LOCATION);
+		lcd.print("   ");
+		lcd.print(humidity, 1);
+		lcd.print("%");
+		lastSecond = second();
+	}
+}
+
+void sendToXbee() {
+	int serialTemp = (int)(temperature * 100);
+	int serialHumidity = (int)(humidity * 100);
+	if (status == 0) {
+
+		Serial1.write("T");
+		Serial1.write(serialTemp >> 8);
+		Serial1.write(serialTemp & 0xff);
+		Serial1.write("H");
+		Serial1.write(serialHumidity >> 8);
+		Serial1.write(serialHumidity & 0xff);
+	}
+	else {
+		Serial1.write("E");
+		Serial1.write(status);
+	}
+}
+
+void readSensor() {
+	status = DHT.read22(DHT22_PIN);
+}
+
+void readSerial() {
+
+	while (Serial1.available() > 0) {
+		unsigned long data = Serial1.read();
+		
+		// 2 identifies clock update
+		if (data == 2) {
+			unsigned long time = 0;
+			for (int i = 3; i >= 0; i--) {
+				data = Serial1.read();
+				time = time | data << (8 * i);
+			}
+			
+			setTime(time);
+		}
+
+		if (data == 1) {
+			do{
+				byte data = Serial1.read();
+				int i = 0;
+				EEPROM.write(i++, data);
+			} while (data != 4);
+		}
+		
+	}
+}
+
+void loop()
+{
+	if (millis() -lastRead > 3000) {
+		readSensor();
+		temperature = DHT.temperature;
+		humidity = DHT.humidity;
+		sendToXbee();
+		lastRead = millis();
+	}
+
+	readSerial();
+	updateLCD();
+}
